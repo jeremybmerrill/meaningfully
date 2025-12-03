6f254b364dda30b81587e25619ea96de9c46dc58
Jeremy B Merrill <jeremybmerrill@gmail.com>
2025-12-02 20:43:16 -0500

    revert mistakenly committed change

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -4,15 +4,9 @@ import { App } from '@meaningfully/ui';
 import { mount } from 'svelte'
 import electronApi from './electronApi';
 
-// If on windows, add the disk name basepath (ex: C:/) on top-level router.
-// via https://github.com/EmilTholin/svelte-routing/issues/143
-const basepath = /^\/?[a-zA-Z]+:/.test(window.location.pathname)
-  ? window.location.pathname.substr(0, window.location.pathname.indexOf(":") + 1)
-  : "/";
-
 const app = mount(App, {
   target: document.getElementById('app')!,
-  props: {api: electronApi, basepath: basepath}
+  props: {api: electronApi, basePath: "/"}
 })
 
 export default app;
